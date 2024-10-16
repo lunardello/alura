e80245e2f0c3e146a612edf336634086d4c3abf2
lunardello <isabela.lunardello@gmail.com>
2024-10-16 07:55:39 -0300

    Add files via upload

---
+++ FLASHCARD/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'pessoal',
+    'Qual meu time favorito?',
+    'corinthians'
+)
+
+criaCartao(
+    'pessoal',
+    'qual minha serie favorita?',
+    'Gossip Girl'
+)
+
+criaCartao(
+    'recomendação',
+    'qual meu filme favorito?',
+    'Velozes E Furiosos'
+)
+
+criaCartao(
+    'recomendacão',
+    'qual meu desfile de moda favorito?',
+    'Victoria Secrets'
+)
